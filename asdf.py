3fdb5b93435aab8fb4518e95969b7403f3917101
paulus.germanus <paulus.germanus@gmail.com>
2012-09-30 19:42:50 +0200

    working calc 0.1

--- asdf.py
+++ asdf.py
@@ -1,18 +1,23 @@
 x = input("Give x?")
- print "You have put this in: ", x
+print "You have put this in: ", x
 
 y = input("Give y?")
- print "You have put this in: ", y
+print "You have put this in: ", y
 
 z = raw_input("Give operation")
- while:
-   if z == "+":
-    print x+y
-   if z == "-"
-    print x-y
-   if z == "*"
-    print x*y
-   if z == "/"
-    print x/y
-   else:
-    print('plz give a proper operator')
+while True:
+ if z == "+":
+  print x+y
+  break
+ if z == "-":
+  print x-y
+  break
+ if z == "*":
+  print x*y
+  break
+ if z == "/":
+  print x/y
+  break
+ else:
+  print('plz give a proper operator')
+  break
