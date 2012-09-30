471976654696e32669ad9b69505787b7298027bd
paulus.germanus <paulus.germanus@gmail.com>
2012-09-30 19:26:00 +0200

    question of correctness

--- asdf.py
+++ asdf.py
@@ -1,11 +1,18 @@
 x = input("Give x?")
-print "You have put this in: ", x
+ print "You have put this in: ", x
 
 y = input("Give y?")
-print "You have put this in: ", y
+ print "You have put this in: ", y
 
 z = raw_input("Give operation")
-if z == "+":
- print x+y
-else:
- print('plz give a proper operator')
+ while:
+   if z == "+":
+    print x+y
+   if z == "-"
+    print x-y
+   if z == "*"
+    print x*y
+   if z == "/"
+    print x/y
+   else:
+    print('plz give a proper operator')
