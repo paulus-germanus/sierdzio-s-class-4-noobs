477c51ac2d77794cc1dc59c533503d39da896379
paulus.germanus <paulus.germanus@gmail.com>
2012-10-05 21:21:15 +0200

    coding exercise 01 from lesson on strings

---
+++ string01.py
@@ -0,0 +1,7 @@
+x = input()
+y = x[0]
+z = x[len(x) - 1]
+print(z + (x[1:len(x) - 1] + y))
+
+# this is the answear_program to Coding Exercise: Heads and Tails from the below lesson
+# http://cscircles.cemc.uwaterloo.ca/7a-strings/
